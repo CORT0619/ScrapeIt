bf348cb39b2083a716205eb7fa7829f449a9c594
Crystal <cortney0215@yahoo.com>
2016-08-04 18:24:04 -0400

    changes

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(express.static('public'));
 
 //make connection to db and collection(s)
 	//var db = mongojs('scrapeit', ["scraped"]);
-var uri  = 'mongodb://heroku_xwz9mmg1:hfh9fn7753olc67h9fopv5vfj8@ds145355.mlab.com:45355/heroku_xwz9mmg1';
+var uri  = process.env.MONGODB_URI/* || 'mongodb://heroku_xwz9mmg1:hfh9fn7753olc67h9fopv5vfj8@ds145355.mlab.com:45355/heroku_xwz9mmg1';*/
 
-var db = mongojs(uri);
+var db = mongojs(process.env.MONGODB_URI);
 db.on('error', function(error){
 
 	console.log("DB error: ", error);
